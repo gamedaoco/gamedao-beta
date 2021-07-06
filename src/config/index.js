1cd17db1c61601f68eb5bb4ebacb8d5074b5ff42
2075 <1140449+2075@users.noreply.github.com>
2021-07-07 01:11:27 +0200

    feat(config): add environments

--- src/config/index.js
+++ src/config/index.js
@@ -1,22 +1,23 @@
 import configCommon from './common.json'
 
 const configEnv = require(`./${process.env.NODE_ENV}.json`)
-// const configEnv = require(`./production.json`)
+// const configEnv = require('./production.json')
 
 const types = require('./types.json')
 
 const envVarNames = [
 	'REACT_APP_PROVIDER_SOCKET',
 	'REACT_APP_DEVELOPMENT_KEYRING',
-	'PINATA_URL',
-	'PINATA_KEY',
-	'PINATA_SECRET',
+	'REACT_APP_PINATA_URL',
+	'REACT_APP_PINATA_KEY',
+	'REACT_APP_PINATA_SECRET',
 ]
 
 const envVars = envVarNames.reduce((mem, n) => {
 	if (process.env[n] !== undefined) mem[n.slice(10)] = process.env[n]
 	return mem
 }, {})
+
 
 const config = {
 	...configCommon,
