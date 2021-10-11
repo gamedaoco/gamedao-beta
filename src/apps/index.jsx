2c54ec9663cde00fba3e46b10d6799a7f5062962
2075 <1140449+2075@users.noreply.github.com>
2021-10-11 19:47:45 +0200

    feat(dashboard): update dashboard

--- src/apps/index.jsx
+++ src/apps/index.jsx
@@ -4,22 +4,13 @@
 
 import React, { useEffect, useState, lazy, Suspense } from 'react'
 import { useSubstrate } from '../substrate-lib'
-
-import { Menu, Label, Tab, Grid, Message, Modal, Button, Icon, Image } from 'semantic-ui-react'
-import Loader from '../components/Loader'
-
-import { BiJoystick, BiHomeCircle, BiListCheck, BiListPlus, BiCoinStack, BiCoin, BiPyramid, BiGame, BiPlus, BiDiamond } from 'react-icons/bi'
-
-const DAO = lazy(() => import('./Organisations'))
-const Campaign = lazy(() => import('./Campaigns'))
-const Proposal = lazy(() => import('./Governance'))
-const Tangram = lazy(() => import('./Tangram'))
+import { Loader } from '../components/Loader'
 
 const Dashboard = (props) => {
+
 	const { accountPair } = props
 	const { api } = useSubstrate()
 
-	const [open, setOpen] = useState(true)
 	const [bodies, setBodies] = useState(0)
 	const [campaigns, setCampaigns] = useState(0)
 	const [proposals, setProposals] = useState(0)
@@ -50,112 +41,11 @@ const Dashboard = (props) => {
 		return () => unsubscribe && unsubscribe()
 	}, [api])
 
-	const panes = [
-		{
-			menuItem: (
-				<Menu.Item key="0">
-					<BiHomeCircle /> Organizations
-					{bodies > 0 && (
-						<Label circular color="pink">
-							{bodies}
-						</Label>
-					)}
-				</Menu.Item>
-			),
-			render: () => (
-				<Tab.Pane key="dao">
-					<DAO accountPair={accountPair} />
-				</Tab.Pane>
-			),
-		},
-		{
-			menuItem: (
-				<Menu.Item key="3">
-					<BiCoinStack />
-					Campaigns
-					{campaigns > 0 && (
-						<Label circular color="blue">
-							{campaigns}
-						</Label>
-					)}
-				</Menu.Item>
-			),
-			render: () => (
-				<Tab.Pane key="campaigns">
-					<Campaign accountPair={accountPair} />
-				</Tab.Pane>
-			),
-		},
-		{
-			menuItem: (
-				<Menu.Item key="2">
-					<BiListCheck /> Proposals
-					{proposals > 0 && (
-						<Label circular color="teal">
-							{proposals}
-						</Label>
-					)}
-				</Menu.Item>
-			),
-			render: () => (
-				<Tab.Pane key="proposals">
-					<Proposal accountPair={accountPair} />
-				</Tab.Pane>
-			),
-		},
-		{
-			menuItem: (
-				<Menu.Item key="5">
-					{' '}
-					<BiDiamond /> Tangram{' '}
-				</Menu.Item>
-			),
-			render: () => (
-				<Tab.Pane key="tangram">
-					<Tangram accountPair={accountPair} />
-				</Tab.Pane>
-			),
-		},
-	]
-
 	return (
-		<Grid.Column width={16}>
-			<Suspense fallback={<Loader text="Loading..."></Loader>}>
-				<Tab panes={panes} />
-			</Suspense>
-		</Grid.Column>
+		<Suspense fallback={<Loader text="Loading..."></Loader>}>
+			<h1>Hello.</h1>
+		</Suspense>
 	)
-}
-
-const Disclaimer = ({ open, setOpen }) => (
-	<Modal basic onClose={() => setOpen(false)} open={open} size="small" closeOnEscape={true} closeOnDimmerClick={false}>
-		<Modal.Header>How it works</Modal.Header>
-		<Modal.Content>
-			GameDAO is built on zero network powered by{' '}
-			<a href="substrate.dev" target="_blank">
-				substrate
-			</a>
-			. To use it, you will need a running PolkadotJS Extension in your browser:{' '}
-			<a href="https://polkadot.js.org/extension/" target="_blank">
-				https://polkadot.js.org/extension/
-			</a>{' '}
-			as well as an address created inside of it. Furthermore you will need test token to do any transactions. We are running a faucet on our{' '}
-			<a href="https://discord.gg/UJS4N85ukS" target="_blank">
-				Discord Server
-			</a>
-			. Please join and say hello!
-		</Modal.Content>
-		<Modal.Actions>
-			<Button color="green" inverted onClick={() => setOpen(false)}>
-				<Icon name="checkmark" /> I understand
-			</Button>
-		</Modal.Actions>
-	</Modal>
-)
-
-const Intro = (props) => {
-	const [open, setOpen] = useState(true)
-	return <Disclaimer open={open} setOpen={setOpen} />
 }
 
 export default function Dapp(props) {
@@ -165,6 +55,6 @@ export default function Dapp(props) {
 	return api && api.query.gameDaoCrowdfunding ? ( // && accountPair
 		<Dashboard {...props} />
 	) : (
-		<Intro />
+		null
 	)
 }
