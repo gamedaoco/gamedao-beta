00b6be83563f1a6bcdb97c42f9fe691a7603d136
2075 <marco@one.io>
2021-10-26 12:52:45 +0200

    feat(layout): add topbar, wallet

--- src/layouts/default/Header.tsx
+++ src/layouts/default/Header.tsx
@@ -1,86 +1,28 @@
-import React, { useState } from 'react'
-import { NavLink } from 'react-router-dom'
-import { styled } from '@mui/material/styles'
-import { useTheme } from '@mui/styles'
-
-import HomeIcon from '@mui/icons-material/Home'
+import React from 'react'
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
-
-
-import { BiWallet, BiJoystick, BiHomeCircle, BiListCheck, BiListPlus, BiCoinStack, BiCoin, BiPyramid, BiGame, BiPlus, BiDiamond } from "react-icons/bi";
-
-import { Stack, Box, Typography } from 'src/components'
+import { Stack } from 'src/components'
 import AccountSelector from 'src/components/AccountSelector'
 
-const StyledLink = styled(NavLink)(({ theme }) => ({
-	display: 'flex',
-	alignItems: 'center',
-	color: 'white',
-	'& > p': {
-		paddingLeft: '2rem',
-	},
-	minHeight: '24px',
-}))
-
-const imageURL = `${process.env.PUBLIC_URL}/assets/gamedao_tangram.svg`
-
 interface ComponentProps {
-	accountPair?: object
-	setAccountAddress?: Function
-	showNavigation?: boolean
+	showWallet?: boolean
 }
 
-function Main({ accountPair, setAccountAddress, showNavigation }: ComponentProps) {
-	const theme = useTheme()
-	// const linkActiveColor = theme.palette.secondary.main
-
-	const [ isSidebarOpen, setSidebarOpen ] = React.useState(false)
-	const toggleSidebar = () => setSidebarOpen(!isSidebarOpen)
+function Main({ showWallet }: ComponentProps) {
 
 	return (
-		<AppBar color="secondary" position="sticky">
+		<AppBar color="transparent" position="sticky">
 			<Toolbar
-				onMouseEnter={toggleSidebar}
-				onMouseLeave={toggleSidebar}
 				sx={{
-					justifyContent: 'start',
-					alignItems: 'start',
-					py: '2rem',
-					px: '2rem !important',
-					height: '100vh',
-					width: isSidebarOpen ? '18rem' : '6rem',
-					overflow: 'hidden',
-					transition: 'all 0.1s ease;',
+					zIndex: '9000',
+					background: 'none',
+					width: '100%',
+					alignItems: 'right',
+					justifyContent: 'end',
 				}}
 			>
-				<Stack direction="column" justifyContent="start" alignItems="left" spacing={4}>
-					<StyledLink activeStyle={{ color: 'white' }} to="/">
-						<img style={{ height: '2rem' }} alt="GameDAO" src={imageURL} height={32} />
-					</StyledLink>
+					<AccountSelector />
 
-					<AccountSelector setAccountAddress={setAccountAddress} />
-
-					<StyledLink to="/app">
-						<BiGame color="white" /> <Typography>Dashboard</Typography>
-					</StyledLink>
-					<StyledLink activeStyle={{ color: 'white' }} to="/app/organisations">
-						<BiPyramid color="white" /> <Typography>Organisations</Typography>
-					</StyledLink>
-					<StyledLink activeStyle={{ color: 'white' }} to="/app/governance">
-						<BiListCheck color="white" /> <Typography>Governance</Typography>
-					</StyledLink>
-					<StyledLink activeStyle={{ color: 'white' }} to="/app/campaigns">
-						<BiCoin color="white" /> <Typography>Campaigns</Typography>
-					</StyledLink>
-					<StyledLink activeStyle={{ color: 'white' }} to="/app/tangram">
-						<BiDiamond color="white" /> <Typography>Tangram</Typography>
-					</StyledLink>
-					<StyledLink activeStyle={{ color: 'white' }} to="/app/wallet">
-						<BiWallet color="white" /> <Typography>Wallet</Typography>
-					</StyledLink>
-
-				</Stack>
 			</Toolbar>
 		</AppBar>
 	)
--- src/layouts/default/index.tsx
+++ src/layouts/default/index.tsx
@@ -1,34 +1,45 @@
 import { Box, Container } from '@mui/material'
 import Header from './Header'
+import Sidebar from './Sidebar'
 import Footer from './Footer'
 
 interface ComponentProps {
 	showHeader?: boolean
 	showFooter?: boolean
-	showDrawer?: boolean
+	showSidebar?: boolean
 	showNavigation?: boolean
+	showWallet?: boolean
 	children?: React.ReactNode
 }
 
-const Layout = ({ showHeader, showFooter, showDrawer, children, showNavigation }: ComponentProps) => {
+const Layout = ({ showHeader, showFooter, showSidebar, children, showNavigation, showWallet=true }: ComponentProps) => {
 	return (
-		<Box
-			sx={{
-				height: '100%',
-				display: 'flex',
-				flexDirection: 'row',
-			}}
-		>
-			<Box flexGrow={0}>
-				{ showHeader && <Header showNavigation={showNavigation?showNavigation:null} /> }
+        <>
+			<Box
+				sx={{
+					height: '100%',
+					display: 'flex',
+					flexDirection: 'row',
+				}}
+			>
+
+				<Box flexGrow={0}>
+					{ showSidebar && <Sidebar showNavigation={showNavigation?showNavigation:null} /> }
+				</Box>
+
+				<Box flexGrow={1}>
+					{ showHeader && <Header showWallet={showWallet?showWallet:null} /> }
+					<Container>
+						<Box sx={{ m: '2em', minHeight: '95vh' }}>{children}</Box>
+					</Container>
+				</Box>
+
 			</Box>
+
 			<Box flexGrow={1}>
-				<Container>
-					<Box sx={{ m: '2em', minHeight: '95vh' }}>{children}</Box>
-				</Container>
 				{ showFooter && <Footer/> }
 			</Box>
-		</Box>
+		</>
 	)
 }
 
