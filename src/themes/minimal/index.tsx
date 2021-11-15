7ce233ee7969101c63f3a05b80c027e55bcdfdea
soundyogi <soundyogi@gmail.com>
2021-11-15 04:17:51 +0100

    fix: font italic, bold etc.

--- src/themes/minimal/index.tsx
+++ src/themes/minimal/index.tsx
@@ -9,7 +9,10 @@ import breakpoints from './breakpoints'
 import componentsOverride from './overrides'
 import shadows, { customShadows } from './shadows'
 
-const font = require('../NotoSans-Regular.ttf')
+const regular = require('../NotoSans-Regular.ttf')
+const bold = require('../NotoSans-Bold.ttf')
+const italic = require('../NotoSans-Italic.ttf')
+const bolditalic = require('../NotoSans-BoldItalic.ttf')
 
 const components = {
     MuiCssBaseline: {
@@ -19,9 +22,36 @@ const components = {
           font-style: normal;
           font-display: swap;
           font-weight: 400;
-          src: url(${font}) format('true-type');
+          src: url(${regular}) format('truetype');
           unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
         }
+
+		@font-face {
+			font-family: 'Noto Sans Bold';
+			font-style: normal;
+			font-display: swap;
+			font-weight: 400;
+			src: url(${bold}) format('truetype');
+			unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
+		  }
+
+		  @font-face {
+			font-family: 'Noto Sans Bold';
+			font-style: normal;
+			font-display: swap;
+			font-weight: 400;
+			src: url(${italic}) format('truetype');
+			unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
+		  }
+
+		  @font-face {
+			font-family: 'Noto Sans Bold';
+			font-style: normal;
+			font-display: swap;
+			font-weight: 400;
+			src: url(${bolditalic}) format('truetype');
+			unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
+		  }
       `,
     },
   }
