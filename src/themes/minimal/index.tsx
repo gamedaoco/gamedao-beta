5a79dd7a694073f79a2b6085a8b9ae0e61515d6f
soundyogi <soundyogi@gmail.com>
2021-11-15 04:11:08 +0100

    feat: add noto sans font

--- src/themes/minimal/index.tsx
+++ src/themes/minimal/index.tsx
@@ -9,6 +9,23 @@ import breakpoints from './breakpoints'
 import componentsOverride from './overrides'
 import shadows, { customShadows } from './shadows'
 
+const font = require('../NotoSans-Regular.ttf')
+
+const components = {
+    MuiCssBaseline: {
+      styleOverrides: `
+        @font-face {
+          font-family: 'Noto Sans';
+          font-style: normal;
+          font-display: swap;
+          font-weight: 400;
+          src: url(${font}) format('true-type');
+          unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
+        }
+      `,
+    },
+  }
+
 export const darkTheme = createTheme({
 	palette: palette.dark,
 	shape,
@@ -16,6 +33,7 @@ export const darkTheme = createTheme({
 	breakpoints,
 	shadows: shadows.dark,
 	customShadows: customShadows.dark,
+	components
 })
 
 darkTheme.components = componentsOverride(darkTheme)
@@ -27,6 +45,7 @@ export const lightTheme = createTheme({
 	breakpoints,
 	shadows: shadows.light,
 	customShadows: customShadows.light,
+	components
 })
 
 lightTheme.components = componentsOverride(lightTheme)
