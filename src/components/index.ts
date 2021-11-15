608466846e1f869dae824d39ca4480bf4313480a
soundyogi <soundyogi@gmail.com>
2021-11-15 04:54:35 +0100

    fix: header nicht transparent, mobile sidebar

--- src/components/index.ts
+++ src/components/index.ts
@@ -43,6 +43,7 @@ export { Icons, FontIcon } from './Icons'
 
 //hooks
 export { useFormControl } from '@mui/material/FormControl'
+export { default as useMediaQuery } from '@mui/material/useMediaQuery';
 
 // styled
 export { styled } from '@mui/material/styles'
