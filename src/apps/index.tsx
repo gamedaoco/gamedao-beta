a5318a9ae6b767dabc6d4bb58c55af3c2a0529ae
DarkNebula0 <andre@zero.io>
2021-11-16 13:49:47 +0100

    Add signer to wallet

--- src/apps/index.tsx
+++ src/apps/index.tsx
@@ -4,10 +4,11 @@ import { useIdentity } from 'src/hooks/useIdentity'
 import { useCrowdfunding } from 'src/hooks/useCrowdfunding'
 import { useApiProvider } from '@substra-hooks/core'
 import { useGameDaoControl } from 'src/hooks/useGameDaoControl'
+import { Button } from 'src/components'
 
 const Dashboard = (props) => {
 	const apiProvider = useApiProvider()
-	const { address } = useWallet()
+	const { address, signer } = useWallet()
 	const [name, setName] = useState('')
 	const [proposals, setProposals] = useState(null)
 	const identity = useIdentity(address)
@@ -40,6 +41,25 @@ const Dashboard = (props) => {
 			<h2>DAOs: {nonce ?? 'Loading...'}</h2>
 			<h2>Campaigns: {campaignsCount ?? 'Loading...'}</h2>
 			<h2>Proposals: {proposals ?? 'Loading...'}</h2>
+			<Button
+				onClick={() => {
+					const tx = apiProvider.tx.balances.transfer(
+						'3RaiU4xRF24Q2qwc3H1TWYXrh11po34WoSaQAADszHshRbYJ',
+						1
+					)
+					tx.signAndSend(address, { signer }, ({ status }) => {
+						if (status.isInBlock) {
+							console.log(`Completed at block hash #${status.asInBlock.toString()}`)
+						} else {
+							console.log(`Current status: ${status.type}`)
+						}
+					}).catch((error: any) => {
+						console.log(':( transaction failed', error)
+					})
+				}}
+			>
+				Send MY Money To Andre
+			</Button>
 		</>
 	)
 }
