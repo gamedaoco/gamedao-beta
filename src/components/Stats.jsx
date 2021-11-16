0b87f9b2da21505ec3559b4bb84129f3ddbf8585
DarkNebula0 <andre@zero.io>
2021-11-16 16:40:03 +0100

    Add TODO and cleanup

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,57 +1,59 @@
 import React from 'react'
 import { Card, Typography } from '.'
 
-const Statistics = () => (
-	<>
-		<Card color="red">
-			<Typography>27</Typography>
-			<Typography>orgs</Typography>
-		</Card>
-		<Card color="orange">
-			<Typography>8</Typography>
-			<Typography>daos</Typography>
-		</Card>
-		<Card color="yellow">
-			<Typography>28</Typography>
-			<Typography>members</Typography>
-		</Card>
-		<Card color="olive">
-			<Typography>7'</Typography>
-			<Typography>motions</Typography>
-		</Card>
-		<Card color="green">
-			<Typography>14</Typography>
-			<Typography>campaigns</Typography>
-		</Card>
-		<Card color="teal">
-			<Typography>82</Typography>
-			<Typography>funds raised</Typography>
-		</Card>
-		<Card color="blue">
-			<Typography>123123</Typography>
-			<Typography>funds shared</Typography>
-		</Card>
-		<Card color="violet">
-			<Typography>22</Typography>
-			<Typography>countries</Typography>
-		</Card>
-		<Card color="purple">
-			<Typography>23</Typography>
-			<Typography>games</Typography>
-		</Card>
-		<Card color="pink">
-			<Typography>15</Typography>
-			<Typography>creators</Typography>
-		</Card>
-		<Card color="brown">
-			<Typography>36</Typography>
-			<Typography>teams</Typography>
-		</Card>
-		<Card color="grey">
-			<Typography>49</Typography>
-			<Typography>licenses</Typography>
-		</Card>
-	</>
-)
+// TODO: Remove?
+// TODO: @2075 I do not think that the component is still used
+const Statistics = () =>
+	null
+	// <>
+	// 	<Card color="red">
+	// 		<Typography>27</Typography>
+	// 		<Typography>orgs</Typography>
+	// 	</Card>
+	// 	<Card color="orange">
+	// 		<Typography>8</Typography>
+	// 		<Typography>daos</Typography>
+	// 	</Card>
+	// 	<Card color="yellow">
+	// 		<Typography>28</Typography>
+	// 		<Typography>members</Typography>
+	// 	</Card>
+	// 	<Card color="olive">
+	// 		<Typography>7'</Typography>
+	// 		<Typography>motions</Typography>
+	// 	</Card>
+	// 	<Card color="green">
+	// 		<Typography>14</Typography>
+	// 		<Typography>campaigns</Typography>
+	// 	</Card>
+	// 	<Card color="teal">
+	// 		<Typography>82</Typography>
+	// 		<Typography>funds raised</Typography>
+	// 	</Card>
+	// 	<Card color="blue">
+	// 		<Typography>123123</Typography>
+	// 		<Typography>funds shared</Typography>
+	// 	</Card>
+	// 	<Card color="violet">
+	// 		<Typography>22</Typography>
+	// 		<Typography>countries</Typography>
+	// 	</Card>
+	// 	<Card color="purple">
+	// 		<Typography>23</Typography>
+	// 		<Typography>games</Typography>
+	// 	</Card>
+	// 	<Card color="pink">
+	// 		<Typography>15</Typography>
+	// 		<Typography>creators</Typography>
+	// 	</Card>
+	// 	<Card color="brown">
+	// 		<Typography>36</Typography>
+	// 		<Typography>teams</Typography>
+	// 	</Card>
+	// 	<Card color="grey">
+	// 		<Typography>49</Typography>
+	// 		<Typography>licenses</Typography>
+	// 	</Card>
+	// </>
 
 export default Statistics
--- src/components/TemplateModule.js
+++ src/components/TemplateModule.js
@@ -1,133 +1,138 @@
 import React, { useEffect, useState } from 'react'
 import { TxButton } from '../substrate-lib/components'
-import { Box, Card  } from '.'
+import { Box, Card } from '.'
 import { useApiProvider } from '@substra-hooks/core'
 import { useWallet } from '../context/Wallet'
 
+// TODO: Remove?
+// TODO: @2075 I do not think that the component is still used
 function Main(props) {
-	const apiProvider = useApiProvider()
-	const { account } = useWallet()
+	return null
+	// const apiProvider = useApiProvider()
+	// const { account } = useWallet()
 
-	const { finalized } = props
+	// const { finalized } = props
 
-	// The transaction submission status
-	const [status, setStatus] = useState('')
+	// // The transaction submission status
+	// const [status, setStatus] = useState('')
 
-	const [nonce, updateNonce] = useState()
-	const [block, setBlock] = useState()
+	// const [nonce, updateNonce] = useState()
+	// const [block, setBlock] = useState()
 
-	const [txData, setTxData] = useState({
-		address: null,
-		title: '',
-		cap: 0,
-		deposit: 0,
-		duration: 0,
-		protocol: 0,
-		governance: 0,
-		cid: '',
-	})
+	// const [txData, setTxData] = useState({
+	// 	address: null,
+	// 	title: '',
+	// 	cap: 0,
+	// 	deposit: 0,
+	// 	duration: 0,
+	// 	protocol: 0,
+	// 	governance: 0,
+	// 	cid: '',
+	// })
 
-	const bestBlock = finalized
-		? apiProvider.derive.chain.bestNumberFinalized
-		: apiProvider.derive.chain.bestNumber
+	// const bestBlock = finalized
+	// 	? apiProvider.derive.chain.bestNumberFinalized
+	// 	: apiProvider.derive.chain.bestNumber
 
-	useEffect(() => {
-		const data = {
-			address: account.address,
-			title: 'cool productname',
-			cap: Math.round(Math.random() * 100000) + 1000000000000,
-			deposit: Math.round(Math.random() * 10),
-			duration: 1000000 + Math.round(Math.random() * 10) * 14400,
-			protocol: Math.round(Math.random() * 5),
-			governance: Math.round(Math.random() * 1),
-			cid: 'cid',
-		}
+	// useEffect(() => {
+	// 	const data = {
+	// 		address: account.address,
+	// 		title: 'cool productname',
+	// 		cap: Math.round(Math.random() * 100000) + 1000000000000,
+	// 		deposit: Math.round(Math.random() * 10),
+	// 		duration: 1000000 + Math.round(Math.random() * 10) * 14400,
+	// 		protocol: Math.round(Math.random() * 5),
+	// 		governance: Math.round(Math.random() * 1),
+	// 		cid: 'cid',
+	// 	}
 
-		// console.log('data',data)
-		setTxData(data)
-	}, [nonce, account])
+	// 	// console.log('data',data)
+	// 	setTxData(data)
+	// }, [nonce, account])
 
-	useEffect(() => {
-		let unsubscribeAll = null
+	// useEffect(() => {
+	// 	let unsubscribeAll = null
 
-		bestBlock((number) => {
-			setBlock(number.toNumber())
-		})
-			.then((unsub) => {
-				unsubscribeAll = unsub
-			})
-			.catch(console.error)
+	// 	bestBlock((number) => {
+	// 		setBlock(number.toNumber())
+	// 	})
+	// 		.then((unsub) => {
+	// 			unsubscribeAll = unsub
+	// 		})
+	// 		.catch(console.error)
 
-		return () => unsubscribeAll && unsubscribeAll()
-	}, [bestBlock])
+	// 	return () => unsubscribeAll && unsubscribeAll()
+	// }, [bestBlock])
 
-	useEffect(() => {
-		if (!apiProvider.query.gameDaoCrowdfunding.nonce) return
-		let unsubscribe
+	// useEffect(() => {
+	// 	if (!apiProvider.query.gameDaoCrowdfunding.nonce) return
+	// 	let unsubscribe
 
-		apiProvider.query.gameDaoCrowdfunding
-			.nonce((n) => {
-				if (n.isNone) {
-					updateNonce('<None>')
-				} else {
-					updateNonce(n.toNumber())
-				}
-			})
-			.then((unsub) => {
-				unsubscribe = unsub
-			})
-			.catch(console.error)
+	// 	apiProvider.query.gameDaoCrowdfunding
+	// 		.nonce((n) => {
+	// 			if (n.isNone) {
+	// 				updateNonce('<None>')
+	// 			} else {
+	// 				updateNonce(n.toNumber())
+	// 			}
+	// 		})
+	// 		.then((unsub) => {
+	// 			unsubscribe = unsub
+	// 		})
+	// 		.catch(console.error)
 
-		return () => unsubscribe && unsubscribe()
-	}, [apiProvider.query.gameDaoCrowdfunding])
+	// 	return () => unsubscribe && unsubscribe()
+	// }, [apiProvider.query.gameDaoCrowdfunding])
 
-	return (
-		<>
-			<h1>Generate Campaign</h1>
+	// return (
+	// 	<>
+	// 		<h1>Generate Campaign</h1>
 
-			<Box>
-				<Card centered>
-					<Box textAlign="center">
-						{nonce}
-					</Box>
-					<Box textAlign="center">
-						{block}
-					</Box>
-				</Card>
-			</Box>
+	// 		<Box>
+	// 			<Card centered>
+	// 				<Box textAlign="center">
+	// 					{nonce}
+	// 				</Box>
+	// 				<Box textAlign="center">
+	// 					{block}
+	// 				</Box>
+	// 			</Card>
+	// 		</Box>
 
-			<form>
-				<Box style={{ textAlign: 'center' }}>
-					<TxButton
-						label="Create Generic Campaign"
-						type="SIGNED-TX"
-						setStatus={setStatus}
-						attrs={{
-							palletRpc: 'gameDaoCrowdfunding',
-							callable: 'create',
-							inputParams: [
-								txData.address,
-								txData.title,
-								txData.cap,
-								txData.deposit,
-								txData.duration,
-								txData.protocol,
-								txData.governance,
-								txData.cid,
-							],
-							paramFields: [true, true, true, true, true, true, true, true],
-						}}
-					/>
-				</Box>
-				<div style={{ overflowWrap: 'break-word' }}>{status}</div>
-			</form>
-		</>
-	)
+	// 		<form>
+	// 			<Box style={{ textAlign: 'center' }}>
+	// 				<TxButton
+	// 					label="Create Generic Campaign"
+	// 					type="SIGNED-TX"
+	// 					setStatus={setStatus}
+	// 					attrs={{
+	// 						palletRpc: 'gameDaoCrowdfunding',
+	// 						callable: 'create',
+	// 						inputParams: [
+	// 							txData.address,
+	// 							txData.title,
+	// 							txData.cap,
+	// 							txData.deposit,
+	// 							txData.duration,
+	// 							txData.protocol,
+	// 							txData.governance,
+	// 							txData.cid,
+	// 						],
+	// 						paramFields: [true, true, true, true, true, true, true, true],
+	// 					}}
+	// 				/>
+	// 			</Box>
+	// 			<div style={{ overflowWrap: 'break-word' }}>{status}</div>
+	// 		</form>
+	// 	</>
+	// )
 }
-
+// TODO: Remove?
+// TODO: @2075 I do not think that the component is still used
 export default function TemplateModule(props) {
-	const apiProvider = useApiProvider()
-	const { account } = useWallet()
+	return null
+	// const apiProvider = useApiProvider()
+	// const { account } = useWallet()
 
-	return apiProvider.query.gameDaoCrowdfunding && account ? <Main {...props} /> : null
+	// return apiProvider.query.gameDaoCrowdfunding && account ? <Main {...props} /> : null
 }
