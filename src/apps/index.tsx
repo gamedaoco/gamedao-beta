b7a9337172cb10896cba1f122de7fb77a274549e
DarkNebula0 <andre@zero.io>
2021-11-16 01:26:01 +0100

    Add useGameDaoControl hook

--- src/apps/index.tsx
+++ src/apps/index.tsx
@@ -3,15 +3,16 @@ import { useWallet } from '../context/Wallet'
 import { useIdentity } from 'src/hooks/useIdentity'
 import { useCrowdfunding } from 'src/hooks/useCrowdfunding'
 import { useApiProvider } from '@substra-hooks/core'
+import { useGameDaoControl } from 'src/hooks/useGameDaoControl'
 
 const Dashboard = (props) => {
 	const apiProvider = useApiProvider()
 	const { address } = useWallet()
 	const [name, setName] = useState('')
-	const [bodies, setBodies] = useState(null)
 	const [proposals, setProposals] = useState(null)
 	const identity = useIdentity(address)
 	const { campaignsCount } = useCrowdfunding()
+	const { nonce } = useGameDaoControl()
 
 	useEffect(() => {
 		setName(identity?.toHuman()?.info?.display?.Raw ?? '')
@@ -22,13 +23,9 @@ const Dashboard = (props) => {
 		let unsubscribe = null
 
 		apiProvider
-			.queryMulti(
-				[apiProvider.query.gameDaoControl.nonce, apiProvider.query.gameDaoGovernance.nonce],
-				([bodies, proposals]) => {
-					setBodies((bodies as any).toNumber())
-					setProposals((proposals as any).toNumber())
-				}
-			)
+			.queryMulti([apiProvider.query.gameDaoGovernance.nonce], ([proposals]) => {
+				setProposals((proposals as any).toNumber())
+			})
 			.then((unsub) => {
 				unsubscribe = unsub
 			})
@@ -40,7 +37,7 @@ const Dashboard = (props) => {
 	return (
 		<>
 			<h1>Hello {name ?? 'Loading...'}</h1>
-			<h2>DAOs: {bodies ?? 'Loading...'}</h2>
+			<h2>DAOs: {nonce ?? 'Loading...'}</h2>
 			<h2>Campaigns: {campaignsCount ?? 'Loading...'}</h2>
 			<h2>Proposals: {proposals ?? 'Loading...'}</h2>
 		</>
