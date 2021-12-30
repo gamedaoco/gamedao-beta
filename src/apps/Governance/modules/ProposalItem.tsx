cf1a741590960c80845b0b6d7b01cbdecc2c2b53
2075 <marco@one.io>
2021-12-31 00:27:35 +0100

    feat(proposal): refactor items to grid

--- src/apps/Governance/modules/ProposalItem.tsx
+++ src/apps/Governance/modules/ProposalItem.tsx
@@ -3,13 +3,13 @@ import { useNavigate } from 'react-router'
 import moment from 'moment'
 import { blockTime, PROPOSAL_STATE_MAPPING } from 'src/apps/lib/data'
 import { normalizeNumber } from 'src/utils/normalizeNumber'
-
+import { proposal_states } from '../../lib/data'
 import { useGameDaoControl } from 'src/hooks/useGameDaoControl'
 import { useGameDaoGovernance } from 'src/hooks/useGameDaoGovernance'
 import { useBlock } from 'src/hooks/useBlock'
 
 import { Divider, LinearProgress } from '@mui/material'
-import { Box, Button, Stack, Typography, Chip, useTheme } from 'src/components'
+import { Box, Button, Stack, Typography, Chip, useTheme, Grid } from 'src/components'
 
 export function ProposalItem({ proposal, showDivider }) {
 	const blockNumber = useBlock()
@@ -45,59 +45,78 @@ export function ProposalItem({ proposal, showDivider }) {
 	// Helper functions
 	const calculatePercentage = (count) => (voteCount === 0 ? 0 : (count * 100) / voteCount)
 
+	const proposalStateToString = (state) =>
+		proposal_states?.find( ( s ) => +s.value === state ).text
+
+
 	return proposalMeta && body ? (
 		<>
 			<Box display="flex">
-				<Stack width="100%" display="flex" flexDirection="row" justifyItems="center">
-					<Box
-						flexBasis="45%"
-						onClick={() => navigate(`/app/governance/${proposalId}`)}
-						sx={{ cursor: 'pointer' }}
-					>
-						<Typography variant="h6">{proposalMeta.title}</Typography>
-						<Typography variant="body1">
-							{body.name},{' '}
-							{expires < 0
-								? 'Expired'
-								: `Expires ${moment()
-										.add(expires, 'seconds')
-										.format('MMMM D, YYYY')}`}
-						</Typography>
-					</Box>
-					<Box display="flex" flexBasis="30%" alignItems="center">
-						<Box display="flex" justifyContent="center" flexDirection="column">
-							<Typography>No</Typography>
-							<Typography>{Math.round(calculatePercentage(noCount))}%</Typography>
-						</Box>
-						<LinearProgress
-							sx={{ width: '100%', margin: '0 1rem' }}
-							variant="determinate"
-							value={calculatePercentage(noCount)}
-						/>
-						<Box display="flex" justifyContent="center" flexDirection="column">
-							<Typography>Yes</Typography>
-							<Typography>
-								{Math.round(calculatePercentage(yesCount))}%{}
+
+				<Grid container spacing={2}>
+
+					<Grid item xs={12} md={4} justifyContent="center"    direction="column"
+    display="flex">
+						<Box
+							onClick={() => navigate(`/app/governance/${proposalId}`)}
+							sx={{ cursor: 'pointer' }}
+						>
+							<Typography sx={{ fontSize: '0.75rem', opacity: 0.3 }} variant="body1">{body.name}</Typography>
+							<Typography variant="body1">{proposalMeta.title}</Typography>
+							<Typography sx={{ fontSize: '0.75rem' }} variant="body1">
+								{expires < 0
+									? '' // proposalStateToString(proposalState)
+									: `Expires ${moment()
+											.add(expires, 'seconds')
+											.format('MMMM D, YYYY')}`}
 							</Typography>
 						</Box>
-					</Box>
-					<Box marginLeft="auto" marginRight={1} display="flex" alignItems="center">
-						<Chip
-							sx={{
-								textTransform: 'capitalize',
-								color: proposalStateColor,
-								borderColor: proposalStateColor,
-							}}
-							variant="outlined"
-							label={PROPOSAL_STATE_MAPPING[proposalState]}
-						/>
-					</Box>
-					<Box>
-						<Button onClick={() => navigate(`/app/governance/${proposalId}`)}>
-							{proposalState !== 1 ? 'See Details' : 'Vote'}
-						</Button>
-					</Box>
-				</Stack>
+					</Grid>
+					<Grid item xs={12} md={4} justifyContent="center"    direction="column"
+    display="flex">
+						<Box display="flex" flexBasis="30%" alignItems="center">
+							<Box display="flex" justifyContent="center" flexDirection="column">
+								<Typography>No</Typography>
+								<Typography>{Math.round(calculatePercentage(noCount))}%</Typography>
+							</Box>
+							<LinearProgress
+								sx={{ width: '100%', margin: '0 1rem' }}
+								variant="determinate"
+								value={calculatePercentage(noCount)}
+							/>
+							<Box display="flex" justifyContent="center" flexDirection="column">
+								<Typography>Yes</Typography>
+								<Typography>
+									{Math.round(calculatePercentage(yesCount))}%{}
+								</Typography>
+							</Box>
+						</Box>
+					</Grid>
+					<Grid item xs={6} md={2} justifyContent="center"    direction="column"
+    display="flex">
+						<Box sx={{ verticalAlign: 'middle'}} marginLeft="auto" marginRight={1} display="flex" alignItems="center">
+							<Chip
+								sx={{
+									textTransform: 'capitalize',
+									color: proposalStateColor,
+									borderColor: proposalStateColor,
+								}}
+								variant="outlined"
+								label={PROPOSAL_STATE_MAPPING[proposalState]}
+							/>
+						</Box>
+					</Grid>
+					<Grid item xs={6} md={2} justifyContent="center"    direction="column"
+    display="flex">
+						<Box sx={{ verticalAlign: 'middle'}}>
+							<Button size="large" onClick={() => navigate(`/app/governance/${proposalId}`)}>
+								{proposalState !== 1 ? 'Details' : 'Vote'}
+							</Button>
+						</Box>
+					</Grid>
+
+				</Grid>
+
 			</Box>
 			{showDivider ? <Divider sx={{ margin: '2rem 0' }} /> : null}
 		</>
