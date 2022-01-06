9d78bbdfef9c1b93f50d5fc9cb39c4ff9bd07262
DarkNebula0 <andre@schnelzer.io>
2022-01-06 17:34:31 +0100

    Fix contribute to own campaign

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,5 +1,22 @@
 import { BigNumber } from 'bignumber.js'
+import { decodeAddress, encodeAddress } from '@polkadot/util-crypto'
+import { hexToU8a, isHex } from '@polkadot/util'
 
 export function formatZero(amount): string {
 	return (amount = new BigNumber(amount).multipliedBy(new BigNumber(10).pow(18))).toString()
+}
+
+export function compareAddress(a, b) {
+	const aEnc = encodeAddress(
+		isHex(a)
+			? hexToU8a(a)
+			: decodeAddress(a),
+	);
+	const bEnc = encodeAddress(
+		isHex(b)
+			? hexToU8a(b)
+			: decodeAddress(b),
+	);
+
+	return aEnc === bEnc;
 }
