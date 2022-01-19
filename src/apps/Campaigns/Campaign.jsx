ffb89b5f6b724a80372a6099cb6bd8753a4b681b
soundyogi <soundyogi@gmail.com>
2022-01-19 18:27:33 +0100

    remove: unused tabs and participate button from campaigns

--- src/apps/Campaigns/Campaign.jsx
+++ src/apps/Campaigns/Campaign.jsx
@@ -274,13 +274,13 @@ export function Campaign() {
 										sx={{ mr: 5 }}
 									/>
 								</Box>
-								<ParticipateButton
-									onClick={() => createInfoNotification('Not Implemented Yet!')}
+								{ funded && <ParticipateButton
+									/*onClick={() => createInfoNotification('Not Implemented Yet!')}*/
 									variant={'contained'}
 								>
-									{!funded && 'Participate'}
+									{/*!funded && 'Participate'*/}
 									{funded && 'Funded!'}
-								</ParticipateButton>
+								</ParticipateButton>}
 							</Stack>
 						</Grid>
 					</Grid>
@@ -300,7 +300,7 @@ export function Campaign() {
 						<TabPanel value={value} index={0}>
 							<MarkdownViewer markdown={mdown} />
 						</TabPanel>
-						<TabPanel value={value} index={1}>
+						{/*<TabPanel value={value} index={1}>
 							<Rewards />
 						</TabPanel>
 						<TabPanel value={value} index={2}>
@@ -308,7 +308,7 @@ export function Campaign() {
 						</TabPanel>
 						<TabPanel value={value} index={3}>
 							<Funding />
-						</TabPanel>
+						</TabPanel>*/}
 					</Grid>
 				</Grid>
 			</Container>
